3ab6fbae510d428b95768500cd5a37a0b96edb9c
ps-lolfan <pbsinnur@gmail.com>
2023-08-27 15:11:27 +0530

    edit server name and image functionality done

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -50,7 +50,7 @@ export const ServerHeader = ({
 
                 {isAdmin && (
                     <DropdownMenuItem
-                        // onClick={() => onOpen("invite", { server })}
+                        onClick={() => onOpen("editServer", { server })}
                         className="px-3 py-2 text-sm cursor-pointer"
                     >
                         Server Settings
