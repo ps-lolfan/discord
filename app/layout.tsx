009047be9c547ee9caa122246015774de079baf9
ps-lolfan <pbsinnur@gmail.com>
2023-08-27 11:35:16 +0530

    creating servers functionality UI integration done

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Open_Sans } from 'next/font/google'
 
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/providers/theme-provider'
+import { ModalProvider } from '@/components/providers/modal-provider'
+
 import { cn } from '@/lib/utils'
 
 const font = Open_Sans({ subsets: ['latin'] })
@@ -31,6 +33,7 @@ export default function RootLayout({
             enableSystem={false}
             storageKey='discord-theme'
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
