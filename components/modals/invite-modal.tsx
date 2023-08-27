0cebec036a50f28f37eb5c021807b6ba00ed7dee
ps-lolfan <pbsinnur@gmail.com>
2023-08-27 13:27:29 +0530

    generating new invite code done

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -1,26 +1,58 @@
 "use client";
 
+import { Check, Copy, RefreshCw } from "lucide-react";
+
 import {
     Dialog,
     DialogContent,
-    DialogDescription,
     DialogHeader,
     DialogTitle
 } from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
-import { useRouter } from "next/navigation";
 import { useModal } from "@/hooks/use-modal-store";
+import { useOrigin } from "@/hooks/use-origin";
+import { useState } from "react";
+
+import axios from "axios";
 
 export const InviteModal = () => {
 
-    const { isOpen, onClose, type } = useModal();
-    const router = useRouter();
+    const { isOpen, onOpen, onClose, type, data } = useModal();
+    const origin = useOrigin();
 
     const isModalOpen = isOpen && type === "invite";
+    const { server } = data;
 
+    const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
 
+    const [copied, setCopied] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
+    const onCopy = () => {
+        navigator.clipboard.writeText(inviteUrl);
+        setCopied(true);
 
+        setTimeout(() => {
+            setCopied(false);
+        }, 1000)
+    }
+
+    const onNew = async () => {
+        try {
+            setIsLoading(true);
+
+            const response = await axios.patch(`/api/servers/${server?.id}/invite-code`);
+
+            onOpen("invite", { server: response.data });
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
 
     return (
         <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -30,7 +62,36 @@ export const InviteModal = () => {
                         Invite Friends
                     </DialogTitle>
                 </DialogHeader>
-                Invite Modal
+                <div className="p-6">
+                    <Label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                        Server invite link
+                    </Label>
+                    <div className="flex items-center mt-2 gap-x-2">
+                        <Input
+                            disabled={isLoading}
+                            className="bg-zinc-300/50 border-0 focus-visible:rign-0 text-black focus-visible:ring-offset-0"
+                            value={inviteUrl}
+                            readOnly
+                        />
+                        <Button size="icon" onClick={onCopy} disabled={isLoading}
+                        >
+                            {copied
+                                ? <Check className="w-4 h-4" />
+                                : <Copy className="w-4 h-4" />
+                            }
+                        </Button>
+                    </div>
+                    <Button
+                        onClick={onNew}
+                        disabled={isLoading}
+                        variant="link"
+                        size="sm"
+                        className="text-xs text-zinc-500 mt-4"
+                    >
+                        Generate a new link
+                        <RefreshCw className="w-4 h-4 ml-2" />
+                    </Button>
+                </div>
             </DialogContent>
         </Dialog>
     )
